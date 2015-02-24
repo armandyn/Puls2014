ee3d9fcea529456b64f9e7c15c839615fec5d6c8
armandyn <joseagp89@gmail.com>
2015-02-24 17:52:39 -0600

    reorganizacion del proyecto puls2014

---
+++ README.md
@@ -0,0 +1,2 @@
+# Puls2014
+Proyecto de Puls2014 Curso de diseño web Online
---
+++ js/formulario.js
@@ -0,0 +1,41 @@
+var $form = $('#formulario'),
+	$titulo = $('#titulo'),
+	$url = $('#url'),
+	$button = $('#mostrar-form'),
+	$list = $('#contenido'),
+	$post = $('.item').first();
+if(localStorage.getItem('autosave')){
+	$titulo.val(sessionStorage.getItem('titulo'));
+	$url.val(sessionStorage.getItem('url'));
+}
+var id = setInterval(function(){
+	sessionStorage.getItem('titulo',$titulo.val());
+	sessionStorage.getItem('url',$url.val());
+},1000);
+
+function mostrarFormulario(){
+	$form.slideToggle();
+	$list.slideToggle();
+	return false;
+}
+function agregarPost(){
+	var url = $url.val(),
+		titulo = $titulo.val(),
+		$clone = $post.clone();
+
+	$clone.find('.titulo_item a')
+		.text(titulo)
+		.attr('href', url);
+
+	$clone.hide();
+	$list.prepend($clone);
+	mostrarFormulario();
+	$titulo.val('');
+	$url.val('');
+	$clone.fadeIn();
+	return false;
+}
+
+//Eventos
+$button.click(mostrarFormulario);		
+$form.on('submit',agregarPost);
